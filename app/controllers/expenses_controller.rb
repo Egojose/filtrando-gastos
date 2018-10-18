2a99df892b3b06d6fc78b01c6fffecdd3a1357fb
khris <khris56@hotmail.com>
2018-10-18 18:27:54 -0500

    Modificado metodo index del controlador

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,10 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+     @expenses = params[:concept].present? ? @expenses.where("concept LIKE ?", "%#{params[:concept]}%") : @expenses
+     @expenses = params[:category_id].present? ? @expenses.where(category_id: params[:category_id]) : @expenses
   end
+
+  # order("date DESC")
+
 end
