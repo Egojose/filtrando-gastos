34f05aa23546522895883182c7cf321ea0984dda
khris <khris56@hotmail.com>
2018-10-20 11:41:19 -0500

    Corregir el current user expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,17 +2,13 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
-        if signed_in? && @expense_user == current_user
-          @expenses = Expense.order("date DESC")
-          if params[:concept].present?
-            @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
-          end
-          if params[:category_id].present?
-            @expenses = @expenses.where("category_id = ?", params[:category_id])
-          end
-       else
-         @expenses = []
-       end
-  end
+    @expenses = current_user.expenses.order("date DESC")
+    
+    if params[:concept].present?
+    @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    end
+    if params[:category_id].present?
+    @expenses = @expenses.where("category_id = ?", params[:category_id])
+    end
+ end
 end
