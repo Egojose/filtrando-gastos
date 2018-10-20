ca1285aed8c3991c988b56829688c7f79068cfc6
khris <khris56@hotmail.com>
2018-10-20 10:31:00 -0500

    Porfin papi

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,18 +3,16 @@ class ExpensesController < ApplicationController
 
   def index
 
-        puts current_user
-        puts @expense_id
-        if @expense_id == current_user
+        if signed_in? && @expense_user == current_user
           @expenses = Expense.order("date DESC")
-         if params[:concept].present?
-           @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
-         end
-         if params[:category_id].present?
-           @expenses = @expenses.where("category_id = ?", params[:category_id])
-         end
+          if params[:concept].present?
+            @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+          end
+          if params[:category_id].present?
+            @expenses = @expenses.where("category_id = ?", params[:category_id])
+          end
        else
-         redirect_to root_path
+         @expenses = []
        end
   end
 end
