20da63c865cb8f6c186c7860f6a74890508e4ba1
Egojose <josemicrobiologo@gmail.com>
2019-07-12 22:58:35 -0500

    se actualiza index en controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,13 +2,56 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @expenses = current_user.expenses.order("date DESC")
-    
-    if params[:concept].present?
-    @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    if params[:category_id].blank? && params[:concept].blank?
+      @expenses=User.find(current_user.id).expenses.all
+
+    elsif params[:concept] && params[:category_id].blank?
+      concept=params[:concept].split
+      concept.reject!{|x| x.length<=3}
+      expenses_user=User.find(current_user.id).expenses.all
+      @expenses=[]
+      expenses_user.each do |hash|
+          if concept.any? {|w| hash[:concept] =~ /#{w}/ }
+            @expenses << hash
+          end
+      end
+
+      #concept=params[:concept]
+      #expense=Expense.all  Podmeos obtener una array con hash y aplicar ruby
+      #@expenses=expense.select{|x| x[:concept]==concept}
+
+      #Estos dos funcionan solitos
+      #@expenses=Expense.where(concept: "#{params[:concept]}").to_a
+      @current=params[:concept]
+    elsif params[:category_id] && params[:concept].blank?
+
+      @expenses=Expense.where(category_id: params[:category_id], user_id: current_user.id).to_a
+      @current=params[:category_id]
+
+    elsif params[:concept] && params[:category_id]
+
+      @expensescategory=Expense.where(category_id: params[:category_id], user_id: current_user.id).to_a
+
+
+      #@expensesconcept=Expense.where(concept: "#{params[:concept]}").to_a
+      concept=params[:concept].split
+      concept.reject!{|x| x.length<=3}
+      #expense=Expense.all
+      @expensesconcept=[]
+      @expensescategory.each do |hash|
+          if concept.any? {|w| hash[:concept] =~ /#{w}/ }
+            @expensesconcept << hash
+          end
+      end
+
+      @expenses=@expensesconcept
+
+      #@expenses=(@expensescategory|@expensesconcept).sort {|a,b| b.date <=> a.date}
+      @current=params[:category_id]+", "+params[:concept]
+      #@expenses=@expensesunordered.sort {|a,b| b.date <=> a.date}
+
+    else
+      @expenses = Expense.order("date DESC")
     end
-    if params[:category_id].present?
-    @expenses = @expenses.where("category_id = ?", params[:category_id])
-    end
- end
+  end
 end
